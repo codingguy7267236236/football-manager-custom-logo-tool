4380a0fb07f866ec5f0b490a3934d93729d8de38
Michael <158330984+codingguy7267236236@users.noreply.github.com>
2024-07-24 22:13:55 +0100

    Added files

---
+++ fm.py
@@ -0,0 +1,40 @@
+# Importing libraries
+import os
+from PIL import Image
+
+# folder locations
+imgFld = "E:/bc and scott reacts/planets" # images for the teams must map to in game id to display correctly.
+graphics = "E:/Documents/Sports Interactive/Football Manager 2023/graphics/logos/"
+
+# function to resize image graphics for football manager ui
+def ResizeImages():
+    fmTxt = "" # used to hold the config records data
+
+    # going through all images in the folder to resize and then add record
+    for images in os.listdir(imgFld):
+        if(images.endswith(".png")):
+            img = Image.open(f"{imgFld}/{images}")
+
+            # setting size dimensions for the icon and logo of the club
+            logoSize = (180,180)
+            iconSize = (18,18)
+
+            # resizing the original image for both icon and logo files needed in game
+            icon = img.resize(iconSize)
+            icon.save(f"{graphics}smll{images}")
+            logo = img.resize(logoSize)
+            logo.save(f"{graphics}big{images}")
+            
+            # getting club id from image filename (this is because the clubs logo image should be their in-game id)
+            id = images.split(".")[0]
+
+            # adding the config data for setting the 
+            fmTxt += f'<record from="smll{id}" to="graphics/pictures/club/{id}/icon"/>\n'
+            fmTxt += f'<record from="big{id}" to="graphics/pictures/club/{id}/logo"/>\n'
+
+    #saving txt to textfile which values can then be copied and pasted into main config.xml file
+    with open("fm.txt","w") as txt:
+        txt.write(fmTxt)
+        txt.close()
+
+ResizeImages()
